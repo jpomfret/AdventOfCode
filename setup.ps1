df5bb4025552cdb3c54c87c3550ecd282e6aae12
jpomfret <jpomfret7@gmail.com>
2023-12-04 08:05:16 +0000

    setup for each day script

---
+++ setup.ps1
@@ -0,0 +1,12 @@
+param (
+    $year,
+    $day
+)
+
+$FolderName = ('{0}\{1}\Day{2}' -f $PSScriptRoot, $year, $day)
+Write-Output ('create folder: {0}' -f $FolderName)
+
+New-Item -Path $FolderName -ItemType Directory
+New-Item -ItemType File -Path ('{0}\day{1}.ps1' -f $folderName, $day)
+New-Item -ItemType File -Path ('{0}\sample.txt' -f $folderName)
+New-Item -ItemType File -Path ('{0}\input.txt' -f  $folderName)
